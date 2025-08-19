d1a8e3f476a68363ecc74a82859fc15e29e36921
Markus Hergenröder <markus@coding4glory.net>
2025-08-19 13:51:20 +0200

    fixed typo

--- lua/tiny-templates/health.lua
+++ lua/tiny-templates/health.lua
@@ -1,4 +1,22 @@
-local settings = require('tiny-templates.config').settings
+--[[
+tiny-templates.nvim
+Copyright (C) 2025  Markus Hergenröder <markus@coding4glory.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+local settings = require('tiny-templates.config').options
 local function count_files(path)
     local file_counter = 0
     for _, type in vim.fs.dir(path, {}) do
@@ -12,7 +30,7 @@ return {
         for _, path in ipairs(settings.include) do
             if vim.fn.isdirectory(path) == 1 then
                 local files = count_files(path)
-                vim.health.ok(path .. ' exists (' .. files .. ' files')
+                vim.health.ok(path .. ' exists (' .. files .. ' files)')
             else
                 vim.health.warn(path .. ' does not exist')
             end
