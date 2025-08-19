0bcd8423d9d7775dd59dd5d899a5c25c488c42f7
Markus Hergenröder <markus@coding4glory.net>
2025-08-20 01:03:14 +0200

    splitting find out of load_template

--- lua/tiny-templates/core.lua
+++ lua/tiny-templates/core.lua
@@ -20,52 +20,68 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ---@field options TinyTemplateSettings
 local M = {}
 
+---tries to find the full path of the template file
+---@param template string the filename of the template
+---@return string|nil the path if found, otherwise nil
+M.find = function(template)
+	for _, p in ipairs(M.options.include) do
+		local template_path = vim.fs.joinpath(p, template)
+		if vim.uv.fs_stat(template_path) then
+			return template_path
+		end
+	end
+	return nil
+end
+
 ---loads the template according
 ---@param template string the template name to apply
----@param position any the range prefix for the read command, defaults to 0 for the beginning of the file
+---@param position any the line to insert the template, defaults to 0 for first line
 M.load_template = function(template, position)
-    position = position or 0
-    for _, p in ipairs(M.options.include) do
-        local template_path = vim.fs.joinpath(p, template)
-        if vim.uv.fs_stat(template_path) then
-            vim.cmd(position .. "read " .. template_path)
-            vim.b.tiny_template_added = template
-            return
-        end
-    end
-    vim.notify("Template: " .. template .. " not found", vim.log.levels.WARN)
+	position = position or 0
+	local template_path = M.find(template)
+	if template_path ~= nil then
+		vim.cmd(position .. "read " .. template_path)
+		vim.b.tiny_template_added = template
+	else
+		vim.notify("Template: " .. template .. " not found", vim.log.levels.WARN)
+	end
 end
 
 ---initialize the core module
 M.init = function()
-    M.options = require("tiny-templates.config").options
+	M.options = require("tiny-templates.config").options
 
-    local template_group = vim.api.nvim_create_augroup("tiny_templates", { clear = true })
-    if M.options.no_autoload then
-        return
-    end
+	local template_group = vim.api.nvim_create_augroup("tiny_templates",
+        { clear = true })
 
-    for p, t in pairs(M.options.templates) do
-        vim.api.nvim_create_autocmd('BufNewFile', {
-            pattern = p,
-            group = template_group,
-            callback = function(args)
-                M.load_template(t)
-            end,
-        })
-        vim.api.nvim_create_autocmd('BufRead', {
-            pattern = p,
-            group = template_group,
-            callback = function(args)
-                if vim.api.nvim_get_option_value('modifiable', { buf = args.buf }) == false then return end
-                if vim.api.nvim_buf_line_count(args.buf) > 1 then return end
-                M.load_template(t)
-            end,
-        })
+	if M.options.no_autoload then
+		return M
+	end
 
-    end
+	for p, t in pairs(M.options.templates) do
+		vim.api.nvim_create_autocmd("BufNewFile", {
+			pattern = p,
+			group = template_group,
+			callback = function(args)
+				M.load_template(t)
+			end,
+		})
+		vim.api.nvim_create_autocmd("BufRead", {
+			pattern = p,
+			group = template_group,
+			callback = function(args)
+				if vim.api.nvim_get_option_value("modifiable", { buf = args.buf }) == false then
+					return
+				end
+				if vim.api.nvim_buf_line_count(args.buf) > 1 then
+					return
+				end
+				M.load_template(t)
+			end,
+		})
+	end
 
-    return M
+	return M
 end
 
 return M
